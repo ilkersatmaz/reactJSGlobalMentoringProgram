e54a15a34f44a72fa1aba219600dfe91d30dd6f3
ilkersatmaz <ilker.satmaz@outlook.com>
2022-10-25 18:43:37 +0300

    configure babel with linter (eslint)

--- isatmaz_globalmentriong-app-webpack/src/components/App.js
+++ isatmaz_globalmentriong-app-webpack/src/components/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class App extends Component {
     render(){
         return (
             <div>
-                <h1>My React App</h1>
+                <h1>
+                    My React App!
+                </h1>
             </div>
         )
        
